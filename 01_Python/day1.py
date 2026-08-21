8926fdcf0073fc7d30f3c6c321a6bdc069dfd08f
ashwini2110-web <ashwini.ai2110@gmail.com>
2026-08-21 23:55:26 +0530

    day1.py

--- 01_Python/day1.py
+++ 01_Python/day1.py
@@ -1,11 +1,13 @@
 age = 20
-height = 5.6
 name = "Ashwini"
 is_student = True
 print(type(age))
 print(type(name))
+print(type(is_student))
+height = float(input("Enter your height = "))
 a = 10
 b = 20
 print(a + b)
 print(a // b)
 print(a**2)
+print("Height is = ",height)
